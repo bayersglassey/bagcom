bd3d949cb299599a42531955e130e74b1a1b86d1
Ben Ayers-Glassey <bayersglassey@gmail.com>
2021-10-06 23:46:53 -0700

    added nginx.conf (and deploy.sh now updates it)

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,9 @@ set -euo pipefail
 # Set up $IP
 . ./ip.sh
 
+echo "### Updating website files..." >&2
 rsync -v -a --delete -e ssh ./dst/ bagcom@$IP:/srv/bagcom/www/
+
+echo "### Updating nginx..." >&2
+scp nginx.conf root@$IP:/etc/nginx/nginx.conf
+ssh root@$IP 'systemctl restart nginx'
